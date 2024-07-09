e547b39849d54581d4a8161be49cee403ce79c44
sakthisaranprjx <sakthisaransss55@gmail.com>
2024-07-09 16:08:00 +0530

    updated swagger server uri

---
+++ src/main/java/com/sakthi/genericServices/controller/Ping.java
@@ -0,0 +1,15 @@
+package com.sakthi.genericServices.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Ping {
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping(){
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+}
+
