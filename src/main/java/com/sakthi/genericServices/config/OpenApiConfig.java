3e3de0579cf92d441eb835b5ad1f2b1a3dd0cab4
sakthisaranprjx <sakthisaransss55@gmail.com>
2024-07-11 16:27:10 +0530

    updated gateway uri

--- src/main/java/com/sakthi/genericServices/config/OpenApiConfig.java
+++ src/main/java/com/sakthi/genericServices/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.sakthi.genericServices.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,12 +12,13 @@ import java.util.List;
 @Configuration
 public class OpenApiConfig {
 
-
+    @Value("${tracker.gateway.url}")
+    String gatewayuri;
 
     @Bean
     public OpenAPI myOpenAPI() {
         Server devServer = new Server();
-        devServer.setUrl("http://localhost:80");
+        devServer.setUrl(gatewayuri);
         devServer.setDescription("Auth Service");
         return new OpenAPI().servers(List.of(devServer));
     }
