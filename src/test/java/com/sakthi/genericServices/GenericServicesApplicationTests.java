c3a6c57aeac5838a37984b4b2d71e0894b99b770
sakthisaranprjx <sakthisaransss55@gmail.com>
2024-07-25 22:08:02 +0530

    Update GenericServicesApplicationTests.java

--- src/test/java/com/sakthi/genericServices/GenericServicesApplicationTests.java
+++ src/test/java/com/sakthi/genericServices/GenericServicesApplicationTests.java
@@ -1,13 +1,13 @@
-package com.sakthi.genericServices;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GenericServicesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.sakthi.genericServices;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GenericServicesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
